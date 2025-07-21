47c221b27859e05239b623e1d9aec1839b49c820
croptech-sa <croptech.ksa@gmail.com>
2025-07-21 14:59:41 +0300

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -70,7 +70,7 @@ const Navbar: React.FC<NavbarProps> = ({ isDark, toggleTheme }) => {
     ? 'text-white'
     : 'text-black dark:text-white';
 
-  const logoStyle = isScrolled ? { filter: 'none', color: '#234723' } : {};
+  const logoColorClass = isScrolled ? 'fill-[#234723]' : 'fill-current';
 
   return (
     <nav
@@ -81,12 +81,13 @@ const Navbar: React.FC<NavbarProps> = ({ isDark, toggleTheme }) => {
     >
       <div className="flex items-center justify-between py-2">
         <div className="flex items-center">
-          <img
-            src="/logo.svg"
-            alt="شعار الموقع"
-            className={`transition-all duration-300 ${isScrolled ? 'w-20 h-20' : 'w-24 h-24 md:w-28 md:h-28'}`}
-            style={logoStyle}
-          />
+          <svg
+            className={`transition-all duration-300 ${isScrolled ? 'w-20 h-20' : 'w-24 h-24 md:w-28 md:h-28'} ${logoColorClass}`}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 100"
+          >
+            <use href="/logo.svg#main" />
+          </svg>
         </div>
 
         <div className="hidden md:flex items-center space-x-8 space-x-reverse">
